462bc00b65c2decd99efb7838193e3e72270f2e6
klrkescano <escano.klark@gmail.com>
2024-09-09 22:45:03 -0600

    Pushed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>Klark Chester Escano</h1>
+      <a
+        href="https://github.com/Klrkescano/cprg306-assignments"
+        target={'_blank'}
+      >
+        {' '}
+        My GitHub{' '}
+      </a>
+    </div>
+  );
+}
+
+export default StudentInfo;
